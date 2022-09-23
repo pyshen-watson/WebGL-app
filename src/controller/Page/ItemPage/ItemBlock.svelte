c7247f97c115747ea398aee979a4bd7c8fc59c35
pyshen <pyshen.watson@gmail.com>
2022-09-23 11:46:00 +0800

    Update: Material is implemented now

--- src/controller/Page/ItemPage/ItemBlock.svelte
+++ src/controller/Page/ItemPage/ItemBlock.svelte
@@ -1,25 +1,18 @@
 <script lang="ts">
     import type Store from "$class/Store"
     import type { Item } from "$store/ItemStore"
-    import Flex from "$components/Flex.svelte"
 
     import ModelBlock from "./ModelBlock.svelte"
     import ShaderBlock from "./ShaderBlock.svelte"
-    import TranslateBlock from "./TranslateBlock.svelte"
-    import RotateBlock from "./RotateBlock.svelte"
-    import ScaleBlock from "./ScaleBlock.svelte"
+    import MaterialBlock from "./MaterialBlock.svelte"
 
     export let itemStore: Store<Item>
 </script>
 
 <div>
-    <Flex --gap="5px" --align="space-between">
-        <ModelBlock {itemStore}/>
-        <ShaderBlock {itemStore}/>
-    </Flex>
-    <TranslateBlock {itemStore}/>
-    <RotateBlock {itemStore}/>
-    <ScaleBlock {itemStore}/>
+    <ModelBlock {itemStore}/>
+    <ShaderBlock {itemStore}/>
+    <MaterialBlock {itemStore}/>
 </div>
 
 <style lang="scss">
@@ -29,6 +22,8 @@
         height: 70%;
         padding: 1rem;
         border-right: 2px solid rgba(gray, 0.3);
+
+        display: grid;
     }
 
 </style>
---
+++ src/controller/Page/ItemPage/MaterialBlock.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+    import type { Item } from "$store/ItemStore"
+    import type { Writable } from "svelte/store"
+    import type Store from "$class/Store"
+    import Bar from "$components/Bar.svelte"
+
+    export let itemStore:Store<Item>
+    let store:Writable<Item> = itemStore.store
+</script>
+
+<div>
+    <Bar title="Ka" bind:value={$store.material_Ka} range={[-0.5,1.5,0.1]} displayValue={true}/>
+    <Bar title="Kd" bind:value={$store.material_Kd} range={[-0.5,1.5,0.1]} displayValue={true}/>
+    <Bar title="Ks" bind:value={$store.material_Ks} range={[-0.5,1.5,0.1]} displayValue={true}/>
+    <Bar title="Sn" bind:value={$store.material_Shininess} range={[-1,5,0.1]} displayValue={true}/>
+</div>
+
+<style>
+    div{
+        width: 90%;
+        display: grid;
+    }
+</style>
+
